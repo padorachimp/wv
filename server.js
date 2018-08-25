03c700210d616416377e7f6f4ef66c3f50220b95
Glitch (glib-flyingfish) <none>
2018-08-25 21:18:06 +0000

    🎒💦 Checkpoint ./New.html:690870/17 ./server.js:690870/2869

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 'use strict';
 // server.js
 // where your node app starts
-
-// init proje
 const PAGE_ACCESS_TOKEN = process.env.FB_ACCESS_TOKEN;
 const request = require('request');
 const express = require('express');
@@ -10,19 +8,37 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.static('public'));
+app.use(express.static('src'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(request, response) {response.sendFile(__dirname + '/New.html');});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {console.log('Your app is listening on port ' + listener.address().port);});
+
 
 app.get('/webview', (req, res) => {
-  let referer = req.get('Referer');
-if (referer) {
-    if (referer.indexOf('www.messenger.com') >= 0) {
-        res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
-    } else if (referer.indexOf('www.facebook.com') >= 0) {
-        res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
-    }
-    res.sendFile('New.html', {root: __dirname});
-}
   
+  let referer = req.get('Referer');
+  if (referer) {
+          if (referer.indexOf('www.messenger.com') >= 0) {
+              res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
+          } else if (referer.indexOf('www.facebook.com') >= 0) {
+              res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
+          }
+          res.sendFile('New.html', {root: __dirname});
+      }});
+
+
+
+// Sends response messages via the Send API
+
+app.get('/actions', (req, res) => {
+
+  let body = req.query;
+  let response =  `?id=${body.psid}&name=${body.Name}&tel=${body.Tel}&address1=${body.Address}&address2=${body.Town}&city=${body.City}&zip=${body.Zip}&select=${body.select}`;
+  res.status(200).send('Please close this window to return to the conversation thread.');
+  callflowxo(body.psid, response);
 });
 
 
@@ -49,16 +65,6 @@ function callflowxo(sender_psid, response) {
         }
     });
 }
-// Sends response messages via the Send API
-
-app.get('/actions', (req, res) => {
-
-  let body = req.query;
-  console.log(body);
-let response =  `?id=${body.psid}&name=${body.Name}&tel=${body.Tel}&address1=${body.Address}&address2=${body.Town}&city=${body.City}&zip=${body.Zip}&select=${body.select}`;
-res.status(200).send('Please close this window to return to the conversation thread.');
-callflowxo(body.psid, response);
-});
 
 
 
@@ -71,20 +77,3 @@ callflowxo(body.psid, response);
 
 
 
-
-
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('src'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/New.html');
-});
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
