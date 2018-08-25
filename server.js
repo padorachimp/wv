a76ea213772ac6c09539cfc8c29c6d5982f174c8
Glitch (glib-flyingfish) <none>
2018-08-25 21:29:02 +0000

    🎉🌱 Checkpoint ./server.js:690870/106

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.get('/', function(request, response) {response.sendFile(__dirname + '/New.ht
 var listener = app.listen(process.env.PORT, function() {console.log('Your app is listening on port ' + listener.address().port);});
 
 
-app.get('/webview', (req, res) => {
-  
+app.get('/webview', (req, res) => {  
   let referer = req.get('Referer');
   if (referer) {
           if (referer.indexOf('www.messenger.com') >= 0) {
@@ -34,7 +33,6 @@ app.get('/webview', (req, res) => {
 // Sends response messages via the Send API
 
 app.get('/actions', (req, res) => {
-
   let body = req.query;
   let response =  `?id=${body.psid}&name=${body.Name}&tel=${body.Tel}&address1=${body.Address}&address2=${body.Town}&city=${body.City}&zip=${body.Zip}&select=${body.select}`;
   res.status(200).send('Please close this window to return to the conversation thread.');
@@ -44,20 +42,14 @@ app.get('/actions', (req, res) => {
 
 function callflowxo(sender_psid, response) {
     // Construct the message body
-    let request_body = {
-        "recipient": {
-            "id": sender_psid
-        },
-        "message": response
-    };
+    let request_body = {"recipient": { "id": sender_psid},"message": response};
     console.log(request_body);
-    // Send the HTTP request to the Messenger Platform
+    // Send the HTTP request to flowxo
     request({
         "uri": "https://flowxo.com/hooks/a/dvmk78kp"+response,
-        
         "method": "GET",
-       
-    }, (err, res, body) => {
+       },
+        (err, res, body) => {
         if (!err) {
             console.log('message sent!')
         } else {
