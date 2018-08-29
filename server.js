a896c9548e07cb5452608c87573109b3f25a9cfd
Glitch (glib-flyingfish) <none>
2018-08-29 14:23:50 +0000

    🏡🐒 Checkpoint ./package.json:750916/7 ./server.js:750916/18 ./New.html:750916/36

--- server.js
+++ server.js
@@ -12,80 +12,102 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('src'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {response.sendFile(__dirname + '/New.html');});
+app.get('/', function(request, response) {
+	response.sendFile(__dirname + '/New.html');
+});
 
-
-app.get('/webview', (req, res) => {  
-  console.log('a new re"quest made');
-  let referer = req.get('Referer');
-  console.log('ref', referer);
-  console.log('ref', referer.indexOf('staticxx.facebook.com'));
- if (referer) {
-          if (referer.indexOf('www.messenger.com') >= 0) {
-              res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
-          } else if (referer.indexOf('www.facebook.com') >= 0) {
-              res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
-          } else if (referer.indexOf('staticxx.facebook.com') >= 0) {
-            res.setHeader('X-Frame-Options', 'ALLOW-FROM https://staticxx.facebook.com/'); 
-          } else if(referer.indexOf('m.facebook.com') >=0) { 
-            res.setHeader('X-Frame-Options', 'ALLOW-FROM http://m.facebook.com'); 
-          }
-          return res.sendFile(path.join(__dirname+'/New.html'));
-      }
-   res.status(501).send('Something went wrong');
+app.get('/webview', (req, res) => {
+	console.log('a new re"quest made');
+	let referer = req.get('Referer');
+	console.log('ref', referer);
+	console.log('ref', referer.indexOf('staticxx.facebook.com'));
+	if (referer) {
+		if (referer.indexOf('www.messenger.com') >= 0) {
+			res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
+		} else if (referer.indexOf('www.facebook.com') >= 0) {
+			res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
+		} else if (referer.indexOf('staticxx.facebook.com') >= 0) {
+			res.setHeader(
+				'X-Frame-Options',
+				'ALLOW-FROM https://staticxx.facebook.com/'
+			);
+		} else if (referer.indexOf('m.facebook.com') >= 0) {
+			res.setHeader('X-Frame-Options', 'ALLOW-FROM http://m.facebook.com');
+		}
+		return res.sendFile(path.join(__dirname + '/New.html'));
+	}
+	res.status(501).send('Something went wrong');
 });
 
 // Sends response messages via the Send API
 app.get('/actions', (req, res) => {
-  let body = req.query;
-  let response =  `?id=${body.psid}&name=${body.Name}&tel=${body.Tel}&address1=${body.Address}&address2=${body.Town}&city=${body.City}&zip=${body.Zip}&select=${body.select}`;
-  callflowxo(body.psid, response);
-  res.status(200).send('Please close this window to return to the conversation thread.');
+	let body = req.query;
+	let response = `?id=${body.psid}&name=${body.Name}&tel=${body.Tel}&address1=${
+		body.Address
+	}&address2=${body.Town}&city=${body.City}&zip=${body.Zip}&select=${
+		body.select
+	}`;
+	callflowxo(body.psid, response);
+	res
+		.status(200)
+		.send('Please close this window to return to the conversation thread.');
 });
 
-app.post("/logs",(req, res) => {
-   console.log('a new re"quest made for logs');
- let message = req.body.message;
-  let o = req.body.o;
-  if(o){
-   message = message + '\t' + o; 
-  }
-  
-  console.log("***log "+message);
-  var m = new Date();
-var dateString = m.getUTCFullYear() +"/"+ (m.getUTCMonth()+1) +"/"+ m.getUTCDate() + " " + m.getUTCHours() + ":" + m.getUTCMinutes() + ":" + m.getUTCSeconds();
-  message = '\n' + dateString + ' -- ' + message;
-  var fs = require('fs');
-fs.appendFile(__dirname+'/log.txt', message, function(err) {
-    if(err) {
-        return console.log(err);
-    }
+app.post('/logs', (req, res) => {
+	console.log('a new re"quest made for logs');
+	let message = req.body.message;
+	let o = req.body.o;
+	if (o) {
+		message = message + '\t' + o;
+	}
 
-    console.log("The file was saved!");
+	console.log('***log ' + message);
+	var m = new Date();
+	var dateString =
+		m.getUTCFullYear() +
+		'/' +
+		(m.getUTCMonth() + 1) +
+		'/' +
+		m.getUTCDate() +
+		' ' +
+		m.getUTCHours() +
+		':' +
+		m.getUTCMinutes() +
+		':' +
+		m.getUTCSeconds();
+	message = '\n' + dateString + ' -- ' + message + '\n';
+	var fs = require('fs');
+	fs.appendFile(path.join(__dirname + '/log.txt'), message, function(err) {
+		if (err) {
+			return console.log(err);
+		}
+
+		console.log('The file was saved!');
+	});
 });
-
-
-})
 
 function callflowxo(sender_psid, response) {
-    // Construct the message body
-    let request_body = {"recipient": { "id": sender_psid},"message": response};
-    console.log(request_body);
-    // Send the HTTP request to flowxo
-    request({
-        "uri": "https://flowxo.com/hooks/a/6km6a7md"+response,
-        "method": "GET",
-       },
-        (err, res, body) => {
-      console.log('body', body);
-        if (!err) {
-            console.log('message sent!')
-        } else {
-            console.error("Unable to send message:" + err);
-        }
-    });
+	// Construct the message body
+	let request_body = { recipient: { id: sender_psid }, message: response };
+	console.log(request_body);
+	// Send the HTTP request to flowxo
+	request(
+		{
+			uri: 'https://flowxo.com/hooks/a/6km6a7md' + response,
+			method: 'GET'
+		},
+		(err, res, body) => {
+			console.log('body', body);
+			if (!err) {
+				console.log('message sent!');
+			} else {
+				console.error('Unable to send message:' + err);
+			}
+		}
+	);
 }
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function() {console.log('Your app is listening on port ' + listener.address().port);});
-
+var listener = app.listen(process.env.PORT, function() {
+	console.log('Your app is listening on port ' + listener.address().port);
+});
