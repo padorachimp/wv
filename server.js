f03553d6ca7e935bd6e37b734fad931e987c5bc1
Glitch (glib-flyingfish) <none>
2018-09-16 22:53:22 +0000

    🍘🚋 Checkpoint ./ThailandAddress/minidata.json:690870/921192 ./server.js:690870/4

--- server.js
+++ server.js
@@ -109,5 +109,5 @@ var listener = app.listen(process.env.PORT, function() {
 //const Googledata = require('./ThailandAddress/GetData.js');
 //Googledata.search().then((data=>{console.log(data);}));
 const fs = require('fs');
-var data = fs.readFileSync('./ThailandAddress/data.json','utf8');
+var data = fs.readFileSync('./ThailandAddress/minidata.json','utf8');
 console.log(data);
